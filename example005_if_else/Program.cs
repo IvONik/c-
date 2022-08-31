1576e513d7fa6750a4784d0ef3b69435ea9ec771
Olga <ola.noskova@mail.ru>
2022-08-31 11:10:32 +0300

    commit

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username=Console.ReadLine();
+
+if(username.ToLower()=="маша") //ToLower позволяет все буквы первести в нижний регистр, тогда как ни напиши МАша или МАША, будет выдавать правильный ответ
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
